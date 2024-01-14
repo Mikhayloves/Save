0a9f0dfc338867f597243b85c6f5218b37aaa90f
Neto test <toni_mikhaylovskiy@mail.ru>
2024-01-14 15:51:33 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.List;
+import java.util.function.ToDoubleBiFunction;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -7,41 +8,64 @@ import java.util.zip.ZipOutputStream;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        List<String> list = List.of("C:Games/savegames/save.dat",
-                "C:Games/savegames/save1.dat",
-                "C:Games/savegames/save2.dat");
-        GameProgress gameProgress = new GameProgress(94,10,2,254.3);
-        GameProgress gameProgress1 = new GameProgress(80,20,30,200);
-        GameProgress gameProgress2 = new GameProgress(91,35,10,158);
-        saveGames("C:Games/savegames/save.dat",gameProgress);
-        saveGames("C:Games/savegames/save1.dat", gameProgress1);
-        saveGames("C:Games/savegames/save2.dat", gameProgress2);
-        for (int i = 0; i < list.size(); i++) {
-            zipFiles(list.get(i));
-        }
+    public static void main(String[] args) throws IOException {
+        List<String> list = List.of("C:/Games/save.dat", "C:/Games/save1.dat", "C:/Games/save2.dat");
+        GameProgress gameProgress = new GameProgress(94, 10, 2, 254.3);
+        GameProgress gameProgress1 = new GameProgress(80, 20, 30, 200);
+        GameProgress gameProgress2 = new GameProgress(91, 35, 10, 158);
+        saveGames("C:/Games/save.dat", gameProgress);
+        saveGames("C:/Games/save1.dat", gameProgress1);
+        saveGames("C:/Games/save2.dat", gameProgress2);
+        zipFiles("C:/Games/zip_out.zip", list);
+        deleteFile("C:/Games/save.dat");
+        deleteFile("C:/Games/save1.dat");
+        deleteFile("C:/Games/save2.dat");
 
 
     }
-        public static void saveGames(String way,GameProgress gameProgress) {
-            try (FileOutputStream fileOutputStream = new FileOutputStream(way);
-                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
-                objectOutputStream.writeObject(gameProgress);
-            } catch (Exception exception) {
-                System.out.println(exception.getMessage());
-            }
+
+    public static void saveGames(String way, GameProgress gameProgress) throws IOException {
+        var file = new File(way);
+        if (file.createNewFile()) ;
+        try (FileOutputStream fileOutputStream = new FileOutputStream(way);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(gameProgress);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
         }
-        public static void zipFiles(String way)  {
-            try(ZipOutputStream zos = new ZipOutputStream(new FileOutputStream("zip_out.zip"));
-            FileInputStream fis = new FileInputStream(way)){
-                ZipEntry entry = new ZipEntry(way);
-                zos.putNextEntry(entry);
-                byte[] buffer = new byte[fis.available()];
-                fis.read(buffer);
-                zos.write(buffer);
-                zos.closeEntry();
-            } catch (IOException exception) {
-                System.out.println(exception.getMessage());
+    }
+
+    public static void zipFiles(String outputZipFilePath, List<String> filePaths) {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(outputZipFilePath))) {
+            for (int i = 0; i < filePaths.size(); i++) {
+                File file = new File(filePaths.get(i));
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    ZipEntry entry = new ZipEntry(file.getName());
+                    zos.putNextEntry(entry);
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+
+                    zos.closeEntry();
+                }
             }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
+    }
+
+    static void deleteFile(String delete) {
+        File file = new File(delete); {
+            for (int i = 0; i < file.length(); i++) {
+                if (file.delete()){
+                    System.out.println("Файлы вне папки удалены");
+            } else {
+                    System.out.println("Файлов нет");
+                }
+           }
+        }
+    }
 }
